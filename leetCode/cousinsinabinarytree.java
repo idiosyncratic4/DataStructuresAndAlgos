c288064207475f2116d1dd8dd039b5de020bef44
nandini sharma <46066660+nansha3120@users.noreply.github.com>
2020-05-07 23:17:39 +0530

    Create cousinsinabinarytree.java

---
+++ leetCode/cousinsinabinarytree.java
@@ -0,0 +1,23 @@
+class Solution {
+     TreeNode xParent = null;
+    TreeNode yParent = null;
+    int xDepth = -1, yDepth = -1;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        findDepthAndParent(root, x, y, 0, null);
+        return xDepth == yDepth && xParent != yParent;
+    }
+    public void findDepthAndParent(TreeNode root, int x, int y, int depth, TreeNode parent){
+        if(root == null){
+            return;
+        }
+        if(root.val == x){
+            xParent = parent;
+            xDepth = depth;
+        }else if(root.val == y){
+            yParent = parent;
+            yDepth = depth;
+        } else {     
+        findDepthAndParent(root.left, x, y, depth + 1, root);
+        findDepthAndParent(root.right, x, y, depth + 1, root); }
+    }
+}
