1c513e2114d7f1164f69cce92855e005622856f2
nandini sharma <46066660+nansha3120@users.noreply.github.com>
2020-05-17 14:18:31 +0530

    Create lifeuni.c

---
+++ CodeChef/LRNDSA01/lifeuni.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t=0;
+	while(t!=42)
+	{
+	    scanf("%i",&t);
+	    if(t!=42)
+	    {
+	        printf("%i",&t);
+	    }
+	}
+	return 0;
+}
+
