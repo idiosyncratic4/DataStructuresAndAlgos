a499f0000eff9dbeec0195d648ad936ecdafe392
nandini sharma <46066660+nansha3120@users.noreply.github.com>
2020-05-17 18:27:07 +0530

    Update coinflip.cpp

--- CodeChef/LRNDSA01/coinflip.cpp
+++ CodeChef/LRNDSA01/coinflip.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
- int t, g, n, q, i;
- scanf("%d", &t);
- while(t--) {
-  scanf("%d", &g);
-  while(g--) {
-   scanf("%d %d %d", &i, &n, &q);
-   if(n%2==0) printf("%d\n", n/2);
+ int T, G, N, Q, I;
+ scanf("%d", &T);
+ while(T--) {
+  scanf("%d", &G);
+  while(G--) {
+   scanf("%d %d %d", &I, &N, &Q);
+   if(N%2==0) printf("%d\n", N/2);
    else {
-    if(i==q) printf("%d\n", (n-1)/2);
-    else printf("%d\n", (n+1)/2);
+    if(I==Q) printf("%d\n", (N-1)/2);
+    else printf("%d\n", (N+1)/2);
    }
   }
  }
