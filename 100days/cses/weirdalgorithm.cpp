a13f8996a964ec1a112c8369e88bc32edc3a1bcc
nandini sharma <46066660+idiosyncratic4@users.noreply.github.com>
2020-06-28 02:09:08 +0530

    Update weirdalgorithm.cpp

--- 100days/cses/weirdalgorithm.cpp
+++ 100days/cses/weirdalgorithm.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main()
 {
+// 	#ifndef ONLINE_JUDGE
+// freopen("input.txt","r",stdin);
+// freopen("output.txt", "w", stdout);
+// #endif
     long long  n;
     cin>>n;
     while(n!=1)
@@ -11,4 +15,6 @@ int main()
         else if(n%2==0){ n=n/2;}
         else { n=n*3+1;}
     }
+    cout<<1;
+    return 0;
 }
