b3aedcc5f1d624c908bdba124a93c4e543425f29
Nandini Sharma <46066660+idiosyncratic4@users.noreply.github.com>
2020-07-09 15:15:26 +0530

    Update BitString.cpp

--- 100days/cses/BitString.cpp
+++ 100days/cses/BitString.cpp
@@ -1,8 +1,6 @@
+
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-using namespace std;
-typedef long long ll;
 #define Mod 1000000007
 
 int main() {
@@ -10,8 +8,9 @@ int main() {
 freopen("input.txt","r",stdin);
 freopen("output.txt", "w", stdout);
 #endif*/
-  ll n; cin>>n;
-  ll result =1 ;
+  long long n;
+  cin>>n;
+  long long result =1 ;
   while(n--)
   {
   	result = (2*(result%Mod))%Mod;
