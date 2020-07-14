c773a5b9f05abc1d258f39a51e34409b6c6907cb
Nandini Sharma <46066660+idiosyncratic4@users.noreply.github.com>
2020-07-14 16:38:17 +0530

    Create Start01.cpp

---
+++ CodeChef/Start01.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long n;
+int main() {
+// #ifndef ONLINE_JUDGE
+// freopen("input.txt","r",stdin);
+// freopen("output.txt", "w", stdout);
+// #endif
+  long long n;
+  cin>>n;
+  cout<<n<<"\n";
+}
